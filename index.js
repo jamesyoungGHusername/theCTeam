6048990b12a945e4f85bed8f3c8bdd62c2dea0d7
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-08-30 17:39:25 -0600

    Added css styling, and finished html generation. Needs to be written to the file still

--- index.js
+++ index.js
@@ -10,8 +10,36 @@ let testTeam=new Team();
 
 console.log(testTeam);
 console.log(testTeam.members);
-testTeam.add(new Intern("testName",1,"test@test.com","hard knocks"))
+testTeam.add(new Intern("testName",1,"test@test.com","hard knocks"));
+testTeam.add(new Manager("testName",2,"test@test.com","221B"));
+testTeam.add(new Intern("testName",3,"test@test.com","jamesyoungGHusername"));
 console.log(testTeam.members);
 
-console.log(testTeam.members[0].getHTMLElement());
 
+console.log(buildPageFor(testTeam));
+
+
+
+function buildPageFor(team){
+    let start=`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=l, initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <main>
+            <section class="employees">
+            <h1>Team Members</h1>
+            <ul>`;
+    let content=team.getMemberHTMLElements();
+    let end=`</ul>
+        </section>
+        </main>
+        </body>
+        </html>`;
+    return (start+content+end);
+}
